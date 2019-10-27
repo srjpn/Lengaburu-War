8fa6e9e4c2730e54c9e240d080c8bb24137fe9cf
Sooraj Parameswaran <soorajp@thoughtworks.com>
2019-10-27 13:10:02 +0530

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,56 +1,58 @@
 var { resourceBasedOnLimit, specialFunction, divideBy2, multiplyBy2 } = require('./lib');
 
-
-const fetchFromPreviousBattalion = ({ reminder, battalionUsed, i, limits, arr, acc }) => {
+const fetchFromPreviousBattalion = ({ reminder, battalionUsed, index, limits, armyToBeSent }) => {
+  let limit = 0;
+  let used = 0;
   if (reminder > 0) {
-    const index = i - 1;
-    ({ reminder, used, limit } = resourceBasedOnLimit(limits[arr[index]], reminder, multiplyBy2));
+    const previousIndex = index - 1;
+    ({ reminder, used, limit } = resourceBasedOnLimit(limits[ORDER_OF_BATTALION[previousIndex]], reminder, multiplyBy2));
     battalionUsed = battalionUsed < multiplyBy2(used) ? 0 : specialFunction(battalionUsed, used, divideBy2);
-    limits[arr[index]] = limit;
-    acc.hasOwnProperty(arr[index]) && (acc[arr[index]] += used);
+    limits[ORDER_OF_BATTALION[previousIndex]] = limit;
+    armyToBeSent.hasOwnProperty(ORDER_OF_BATTALION[previousIndex]) && (armyToBeSent[ORDER_OF_BATTALION[previousIndex]] += used);
   }
-  return { reminder, battalionUsed, i, limits, arr, acc };
+  return { reminder, battalionUsed, index, limits, armyToBeSent };
 };
 
-const fetchFromNextBattalion = ({ reminder, battalionUsed, i, limits, arr, acc }) => {
+const fetchFromNextBattalion = ({ reminder, battalionUsed, index, limits, armyToBeSent }) => {
+  let limit = 0;
+  let used = 0;
   if (reminder > 0) {
-    const index = i + 1;
-    ({ reminder, used, limit } = resourceBasedOnLimit(limits[arr[index]], reminder, divideBy2));
+    const nextIndex = index + 1;
+    ({ reminder, used, limit } = resourceBasedOnLimit(limits[ORDER_OF_BATTALION[nextIndex]], reminder, divideBy2));
     battalionUsed = battalionUsed < multiplyBy2(used) ? 0 : specialFunction(battalionUsed, used, multiplyBy2);
-    limits[arr[index]] = limit;
-    acc.hasOwnProperty(arr[index]) && (acc[arr[index]] += used);
+    limits[ORDER_OF_BATTALION[nextIndex]] = limit;
+    armyToBeSent.hasOwnProperty(ORDER_OF_BATTALION[nextIndex]) && (armyToBeSent[ORDER_OF_BATTALION[nextIndex]] += used);
   }
-  return { battalionUsed, reminder, i, limits, arr, acc };
+  return { battalionUsed, reminder, index, limits, armyToBeSent };
 };
 
-const declareIfFailed = ({ reminder, battalionUsed, i, limits, arr, acc }) => {
+const declareIfFailed = ({ reminder, battalionUsed, index, limits, armyToBeSent }) => {
   if (reminder > 0) {
-    acc["Failed"] = true;
+    armyToBeSent["Failed"] = true;
   }
-  return { reminder, battalionUsed, i, limits, arr, acc };
+  return { reminder, battalionUsed, index, limits, armyToBeSent };
 };
 
-const orderOfIteration = ['horse', 'elephant', 'tank', 'sling'];
-
-const actions = [fetchFromPreviousBattalion, fetchFromNextBattalion, declareIfFailed];
-
-module.exports = (armySent) => {
-  var limits = {
-    horse: 100,
-    elephant: 50,
-    tank: 10,
-    sling: 5
-  };
-
-  return orderOfIteration.reduce((acc, x, index, arr) => {
-    var battalionUsed = Math.round(divideBy2(armySent[x]));
-    var reminder = battalionUsed - limits[x] < 0 ? 0 : battalionUsed - limits[x];
-
-    ({ reminder, battalionUsed } = actions
-      .reduce((params, action) => action(params),
-        { reminder, battalionUsed, i: index, limits, arr, acc }));
-    limits[x] = limits[x] - battalionUsed;
-    acc[x] = acc[x] ? acc[x] + (battalionUsed - reminder) : (battalionUsed - reminder);
-    return acc;
-  }, { horse: 0, elephant: 0, tank: 0, sling: 0 });
+const findBattalionCount = (incomingArmy, battalion, ourArmy, index, armyToBeSent) => {
+  var battalionUsed = Math.round(divideBy2(incomingArmy[battalion]));
+  var reminder = battalionUsed - ourArmy[battalion] < 0 ? 0 : battalionUsed - ourArmy[battalion];
+  const initialValue = { reminder, battalionUsed, index, limits: ourArmy, armyToBeSent };
+  ({ reminder, battalionUsed } = ACTIONS.reduce((params, action) => action(params), initialValue));
+  ourArmy[battalion] = ourArmy[battalion] - battalionUsed;
+  armyToBeSent[battalion] = armyToBeSent[battalion] ? armyToBeSent[battalion] + (battalionUsed - reminder) : (battalionUsed - reminder);
+  return armyToBeSent;
 };
+
+const ORDER_OF_BATTALION = ['horse', 'elephant', 'tank', 'sling'];
+
+const ACTIONS = [fetchFromPreviousBattalion, fetchFromNextBattalion, declareIfFailed];
+
+module.exports = (incomingArmy) => {
+  const ourArmy = { horse: 100, elephant: 50, tank: 10, sling: 5 };
+  const armyToBeSent = { horse: 0, elephant: 0, tank: 0, sling: 0 }
+  ORDER_OF_BATTALION
+    .forEach((battalion, index) =>
+      findBattalionCount(incomingArmy, battalion, ourArmy, index, armyToBeSent));
+  return armyToBeSent;
+};
+
